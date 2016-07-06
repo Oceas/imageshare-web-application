7de569f4ba0487181313b3754bc81e7dc56f1df7
oceass <scott8513@gmail.com>
2016-07-05 21:22:49 -0400

    Initial project commit of base angular project

---
+++ imageshare/javascript/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by scottanderson on 6/27/16.
+ */
+
+/*Controllers are where we define our app's behavior by defining
+ functions and values */
+(function () {
+    var app = angular.module('store', ['store-products']);
+
+    
+
+})();
