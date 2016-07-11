3200c05719beeb1b885be3a97ecd26be66567942
oceass <scott8513@gmail.com>
2016-07-10 20:59:14 -0400

    Added final authentication factories

--- imageshare/modules/Authentication.js
+++ imageshare/modules/Authentication.js
@@ -1,7 +1,7 @@
-var authenticationServices = angular.module('authenticationServices',['ngCookies']);
+var imageShareAuthenticationServices = angular.module('authenticationServices',['ngCookies']);
 
-authenticationServices.factory('checkCreds',
-    ['cookies', function($cookies){
+imageShareAuthenticationServices.factory('checkCreds',
+    ['$cookies', function($cookies){
         return function(){
             var returnVal = false;
             var userCreds = $cookies.userCreds;
@@ -9,5 +9,46 @@ authenticationServices.factory('checkCreds',
                 returnVal = true;
             }
             return returnVal;
+        };
+    }]);
+
+imageShareAuthenticationServices.factory('getToken',
+    ['$cookies', function($cookies){
+        return function(){
+            var returnVal = "";
+            var userCreds = $cookies.userCreds;
+            if (userCreds !== undefined && userCreds !== ""){
+                returnVal = btoa(userCreds);
+            }
+            return returnVal;
+        };
+    }]);
+
+imageShareAuthenticationServices.factory('getUsername',
+    ['$cookies', function($cookies){
+        return function(){
+            var returnVal = "";
+            var username = $cookies.username;
+            if (userCreds !== undefined && username !== ""){
+                returnVal = username;
+            }
+            return returnVal;
+        };
+    }]);
+
+imageShareAuthenticationServices.factory('setCreds',
+    ['$cookies', function($cookies){
+        return function(un,pw){
+            var token = un.concat(":",pw);
+            $cookies.userCreds = token;
+            $cookies.username = un;
+        };
+    }]);
+
+imageShareAuthenticationServices.factory('detleteCreds',
+    ['$cookies', function($cookies){
+        return function(){
+            $cookies.userCreds = "";
+            $cookies.username = "";
         };
     }]);
--- imageshare/modules/app.js
+++ imageshare/modules/app.js
@@ -6,32 +6,32 @@
  functions and values */
 
 (function () {
-    var imageshare = angular.module('imageshare', ['ngRoute','authenticationServices']);
+    var imageshare = angular.module('imageshare', ['ngRoute', 'authenticationServices']);
 
-    imageshare.config(function($routeProvider) {
+    imageshare.config(function ($routeProvider) {
         $routeProvider
         // route for the home page
             .when('/', {
-                templateUrl : '../partials/pages/home.html',
-                controller  : 'mainController'
+                templateUrl: '../partials/pages/home.html',
+                controller: 'mainController'
             })
 
             // route for the login page
             .when('/login', {
-                templateUrl : '../partials/pages/login.html',
-                controller  : 'loginController'
+                templateUrl: '../partials/pages/login.html',
+                controller: 'loginController'
             })
 
             // route for the register page
             .when('/register', {
-                templateUrl : '../partials/pages/register.html',
-                controller  : 'registerController'
+                templateUrl: '../partials/pages/register.html',
+                controller: 'registerController'
             })
 
             // route for user logging out
             .when('/logout', {
-                templateUrl : '../partials/pages/logout.html',
-                controller  : 'logOutController'
+                templateUrl: '../partials/pages/logout.html',
+                controller: 'logOutController'
             });
     });
 
@@ -39,16 +39,18 @@
         return {
             restrict: 'E',
             templateUrl: '../partials/navigation/menu.html',
-            controller : 'navigationController'
+            controller: 'navigationController'
         };
     });
 
-    imageshare.controller('mainController', function($scope) {
+    imageshare.controller('mainController', function ($scope) {
         // create a message to display in our view
+
         $scope.message = 'Welcome to Imageshare the best story telling platform!';
+
     });
 
-    imageshare.controller('loginController', function($scope,$http) {
+    imageshare.controller('loginController', function ($scope, $http) {
 
         $scope.login = function () {
             // use $.param jQuery function to serialize data from JSON
@@ -58,16 +60,16 @@
             });
 
             var config = {
-                headers : {
+                headers: {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
                 }
             }
 
             $http.post('http://imageshare.io/api/login.php', data, config)
                 .success(function (data) {
-                    if(data.error){
+                    if (data.error) {
                         alert(data.message);
-                    }else{
+                    } else {
 
                     }
                     console.log(data);
@@ -78,16 +80,15 @@
 
     });
 
-    imageshare.controller('registerController', function($scope) {
+    imageshare.controller('registerController', function ($scope) {
 
     });
 
-    imageshare.controller('logOutController', function($scope) {
+    imageshare.controller('logOutController', function ($scope) {
         $scope.message = 'You are now logged out!';
     });
 
-    imageshare.controller('navigationController', function($scope){
-
+    imageshare.controller('navigationController', function () {
     });
 
 })();
