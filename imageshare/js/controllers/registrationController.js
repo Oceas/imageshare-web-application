8ad0e73a71c7ff89123a1b1f3aaa1a1724dec2a0
oceass <scott8513@gmail.com>
2016-07-15 09:24:00 -0400

    Fixed user registration.

--- imageshare/js/controllers/registrationController.js
+++ imageshare/js/controllers/registrationController.js
@@ -1,4 +1,4 @@
-angular.module('imageshare').controller('registrationController', ['$http', '$scope','$window', function ($window,$http, $scope) {
+angular.module('imageshare').controller('registrationController', ['$http','$scope', function ($http, $scope) {
     $scope.register = function () {
         // use $.param jQuery function to serialize data from JSON
         var data = $.param({
@@ -11,7 +11,7 @@ angular.module('imageshare').controller('registrationController', ['$http', '$sc
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
             }
-        }
+        };
 
         $http.post('http://imageshare.io/api/register.php', data, config)
             .success(function (data) {
@@ -29,4 +29,5 @@ angular.module('imageshare').controller('registrationController', ['$http', '$sc
 
             });
     };
+
 }]);
