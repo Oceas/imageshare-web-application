a357e0155a2b29d005ae68c1aaf308e7618063bf
oceass <scott8513@gmail.com>
2016-07-20 20:50:21 -0400

    Added gitignore. Removed vagrant use files from repo. Resolved issue where a user's username displayed wouldn't update on login.

--- imageshare/js/controllers/mainController.js
+++ imageshare/js/controllers/mainController.js
@@ -1,9 +1,5 @@
 angular.module('imageshare').controller('mainController', ['updateSystemSettings', 'getUsername', '$scope' ,'getUID', function (updateSystemSettings, getUsername, $scope,getUID) {
     // create a message to display in our view
-    if (getUsername !== undefined) {
-        $scope.username = getUsername();
-        $scope.uid = getUID();
-    }
     updateSystemSettings();
     $scope.message = 'Welcome to Imageshare the best story telling platform!';
 }]);
