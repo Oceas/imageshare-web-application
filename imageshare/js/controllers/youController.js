2cab428f90a094168256757fd021f013647eea02
oceass <scott8513@gmail.com>
2016-07-21 22:41:34 -0400

    Added ng-switcher in you page that switches between the different rolls.

--- imageshare/js/controllers/youController.js
+++ imageshare/js/controllers/youController.js
@@ -1,14 +1,14 @@
 angular.module('imageshare').controller('youController', ['$scope',function ($scope) {
 
     //default view to moments
-    $scope.selected = 1;
+    $scope.selection = "photos";
 
     $scope.setSelected = function(view){
-        $scope.selected = view;
+        $scope.selection = view;
     }
 
     $scope.isSelected = function(view){
-        return $scope.selected == view;
+        return $scope.selection == view;
     }
 
 }]);
