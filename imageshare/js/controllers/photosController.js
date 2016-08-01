3ddc6954c400a1114f7091c681f73646416ccaa7
oceas <scott8513@gmail.com>
2016-08-01 02:01:02 -0400

    Photo information now updates when uploaded

--- imageshare/js/controllers/photosController.js
+++ imageshare/js/controllers/photosController.js
@@ -14,9 +14,8 @@ angular.module('imageshare').controller('photosController', ['$rootScope','$scop
         $http.post('http://imageshare.io/api/v1/getalbums.php', data, config)
             .success(function (data) {
                 if (data.error) {
-                    alert("Moments: " + data.message);
+                    console.log("Moments: " + data.message);
                 } else {
-                    console.log(data.albums);
                     $scope.moments = data.albums;
                     $scope.moments.forEach(loadPhotos);
                 }
@@ -40,31 +39,34 @@ angular.module('imageshare').controller('photosController', ['$rootScope','$scop
         $http.post('http://imageshare.io/api/v1/getalbumdetail.php', data, config)
             .success(function (data) {
                 if (data.error) {
-                    alert(data.message);
+                    console.log(data.message);
                 } else {
-                    console.log(data.album.images);
                     $scope.moments[index].photos = data.album.images;
-                    console.log("Loaded photos for " + moment.albumName);
                 }
-                console.log(data);
             })
             .error(function () {
                 console.log("ERROR!");
             });
     };
 
-    loadMoments();
 
     $scope.filesChanged = function (element){
         $scope.file = element.files[0];
         $scope.$apply();
     };
 
-    $scope.upload = function(){
+    $scope.uploadPhoto = function(){
         var data = new FormData();
         data.append('userId', $rootScope.uid);
         data.append('albumId', $scope.momentSelection);
         data.append('fileToUpload[]',$scope.file);
+
+        var photo = {
+            caption: $scope.photoCaption,
+            description: $scope.photoDescription,
+            location: $scope.photoLocation,
+            fileName: $scope.file.name
+        };
 
         var config = {
             transformRequest:angular.identity,
@@ -74,15 +76,74 @@ angular.module('imageshare').controller('photosController', ['$rootScope','$scop
         };
         $http.post('http://imageshare.io/api/v1/uploadimage.php',data,config).success(function (data) {
             if (data.error) {
-                alert("Photo: " + data.message);
-                console.log(data);
+                console.log("Photo: " + data.message);
             } else {
-                console.log(data);
-                alert("Successfully uploaded!");
+                // photo.imageId = getPhotoID($scope.momentSelection,$scope.file.name);
+                updatePhotoInformation($scope.momentSelection,photo);
+                console.log("Successfully uploaded!");
             }
         }).error(function () {
 
         });
-    }
+    };
+
+    var updatePhotoInformation = function(albumId,photo){
+
+        var data = $.param({
+            userId: $rootScope.uid,
+            albumId: albumId
+        });
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+        $http.post('http://imageshare.io/api/v1/getalbumdetail.php', data, config)
+            .success(function (data) {
+                if (data.error) {
+                    console.log(data.message);
+                } else {
+                    var images = data.album.images;
+
+                    photo.imageId = images[images.length - 1].imageId;
+
+                    if(photo.imageId == null){
+                    }else{
+                        console.log("Safe!");
+                        console.log(photo.imageId);
+                    }
+
+
+                    var newData = $.param({
+                        imageId: photo.imageId,
+                        caption: photo.caption,
+                        imageDesc: photo.description,
+                        location: photo.location
+                    });
+
+                    $http.post('http://imageshare.io/api/v1/editimage.php', newData ,config).success(function (data) {
+                        if (data.error) {
+                            console.log(newData);
+                            console.log("Photo Edit: " + data.message);
+                            console.log(data);
+                        } else {
+                            console.log(data);
+                            console.log("Photo Edit: Successfully updated!");
+                        }
+                    }).error(function () {
+
+                    });
+
+                }
+            })
+            .error(function () {
+                console.log("ERROR!");
+            });
+
+
+    };
+
+    loadMoments();
 
 }]);
