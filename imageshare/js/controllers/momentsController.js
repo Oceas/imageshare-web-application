397e12ca2920b954a4c89f5c72c1c8dfa6a2b626
Scott Anderson <scottanderson@Scotts-MacBook-Pro-3.local>
2016-07-31 22:59:29 -0400

    When a moment is now created it is immediately associated with a selected story

--- imageshare/js/controllers/momentsController.js
+++ imageshare/js/controllers/momentsController.js
@@ -1,6 +1,31 @@
 angular.module('imageshare').controller('momentsController', ['$rootScope','$scope','$http', function ($rootScope, $scope, $http) {
     $scope.moments = {};
 
+    $scope.stories = {};
+
+    var loadStories = function() {
+        var data = $.param({
+            userId: $rootScope.uid,
+        });
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+        $http.post('http://imageshare.io/api/v1/getstories.php', data, config)
+            .success(function (data) {
+                if (data.error) {
+                    alert("Stories: " + data.message);
+                } else {
+                    $scope.stories = data.stories;
+                }
+            })
+            .error(function () {
+
+            });
+    };
+
 
     $scope.createMoment = function() {
         var data = $.param({
@@ -21,7 +46,33 @@ angular.module('imageshare').controller('momentsController', ['$rootScope','$sco
                     alert("Create Moment: " + data.message);
                 } else {
                     console.log(data);
+                    addMomentToSory(data.albumId,$scope.storySelection);
                     alert("Moment Created!")
+                }
+            })
+            .error(function () {
+
+            });
+    };
+
+    var addMomentToSory = function (moment, story){
+        var data = $.param({
+            userId: $rootScope.uid,
+            storyId: story,
+            albumId: moment
+        });
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+        $http.post('http://imageshare.io/api/v1/addalbumtostory.php', data, config)
+            .success(function (data) {
+                if (data.error) {
+                    alert("Moments: " + data.message);
+                } else {
+                    console.log(data);
                 }
             })
             .error(function () {
@@ -78,6 +129,7 @@ angular.module('imageshare').controller('momentsController', ['$rootScope','$sco
     };
 
     loadMoments();
+    loadStories();
 
 
 }]);
