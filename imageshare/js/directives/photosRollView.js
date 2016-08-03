5cb533b071e129719fcee52f5185107c4fc56487
oceas <scott8513@gmail.com>
2016-08-03 19:55:30 -0400

    Created photos roll view directive.

---
+++ imageshare/js/directives/photosRollView.js
@@ -0,0 +1,7 @@
+angular.module('imageshare').directive('photosRollView', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '../../partials/pages/photos/rollView.html',
+        controller: 'photosController'
+    };
+});
