f8058ad2bd064b756d8af4ef3ffc24d6591b6c89
oceas <scott8513@gmail.com>
2016-08-03 08:50:48 -0400

    Created Switching between creating and viewing for story, moments, and photos roll.

--- imageshare/js/controllers/youController.js
+++ imageshare/js/controllers/youController.js
@@ -2,6 +2,8 @@ angular.module('imageshare').controller('youController', ['$scope',function ($sc
 
     //default view to moments
     $scope.selection = "stories";
+    $scope.mode = "view";
+
 
     $scope.setSelected = function(view){
         $scope.selection = view;
@@ -9,6 +11,16 @@ angular.module('imageshare').controller('youController', ['$scope',function ($sc
 
     $scope.isSelected = function(view){
         return $scope.selection == view;
+    };
+
+    $scope.setMode = function(view){
+        console.log("Set mode called for" + view);
+        $scope.mode = view;
+    };
+
+    $scope.isMode = function(view){
+        console.log("Is mode called for" + view);
+        return $scope.mode == view;
     };
 
 }]);
