06231681e59d9c85bb6078c0bf477eb1080a1c97
oceas <scott8513@gmail.com>
2016-08-04 19:52:51 -0400

    Completed editing photos functionality. Made deleting all collection types consistent.

--- imageshare/js/controllers/photosController.js
+++ imageshare/js/controllers/photosController.js
@@ -93,9 +93,31 @@ angular.module('imageshare').controller('photosController', ['$rootScope','$scop
             });
     };
 
+    $scope.editPhotoInformation = function(photo){
+        var data = $.param({
+            imageId: photo.imageId,
+            caption: photo.imageCaption,
+            imageDesc: photo.imageDesc,
+            location: photo.location
+        });
 
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+        $http.post('http://imageshare.io/api/v1/editimage.php', data, config)
+            .success(function (data) {
+                if (data.error) {
+                    console.log("Photos Edit: " + data.message);
+                } else {
+                    console.log("Successfully Edited " + photo.imageId);
+                }
+            })
+            .error(function () {
 
-
+            });
+    };
 
     var updatePhotoInformation = function(albumId,photo){
 
