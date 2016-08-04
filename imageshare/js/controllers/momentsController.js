75e842980e7270f30b075cb43418a35320bf5ed6
oceas <scott8513@gmail.com>
2016-08-04 09:02:10 -0400

    Replaced loadStories back for moments creation.

--- imageshare/js/controllers/momentsController.js
+++ imageshare/js/controllers/momentsController.js
@@ -124,6 +124,29 @@ angular.module('imageshare').controller('momentsController', ['$rootScope','$sco
             });
     };
 
+    var loadStories = function() {
+        var data = $.param({
+            userId: $rootScope.uid,
+        });
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+        $http.post('http://imageshare.io/api/v1/getstories.php', data, config)
+            .success(function (data) {
+                if (data.error) {
+                    alert("Stories: " + data.message);
+                } else {
+                    $scope.stories = data.stories;
+                }
+            })
+            .error(function () {
+
+            });
+    };
+
     var loadPhotos = function(moment, index) {
         var data = $.param({
             userId: $rootScope.uid,
@@ -149,6 +172,6 @@ angular.module('imageshare').controller('momentsController', ['$rootScope','$sco
     };
 
     loadMoments();
-
+    loadStories();
 
 }]);
