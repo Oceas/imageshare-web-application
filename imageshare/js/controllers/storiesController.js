f38d14bdbb420ddab3b7376e348dbc5ce8b668f3
oceas <scott8513@gmail.com>
2016-08-04 10:37:38 -0400

    Cleaned up story controller.

--- imageshare/js/controllers/storiesController.js
+++ imageshare/js/controllers/storiesController.js
@@ -2,34 +2,6 @@ angular.module('imageshare').controller('storiesController', ['$rootScope','$sco
 
     $scope.stories = {};
 
-    $scope.createStory = function() {
-        var data = $.param({
-            userId: $rootScope.uid,
-            storyName: $scope.storyName,
-            storyDesc: $scope.storyDesc,
-            location: $scope.location
-        });
-
-        console.log(data);
-
-        // var config = {
-        //     headers: {
-        //         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-        //     }
-        // };
-        // $http.post('http://imageshare.io/api/v1/createstory.php', data, config)
-        //     .success(function (data) {
-        //         if (data.error) {
-        //             alert("Create Story: " + data.message);
-        //         } else {
-        //             alert("Story Created!")
-        //         }
-        //     })
-        //     .error(function () {
-        //
-        //     });
-    };
-
     var loadStories = function() {
         var data = $.param({
             userId: $rootScope.uid,
