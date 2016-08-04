80d6dc26d417cdc541896580f3cb151665a8eabb
oceas <scott8513@gmail.com>
2016-08-04 10:37:00 -0400

    Cleaned up photos controller.

--- imageshare/js/controllers/photosController.js
+++ imageshare/js/controllers/photosController.js
@@ -93,44 +93,8 @@ angular.module('imageshare').controller('photosController', ['$rootScope','$scop
     };
 
 
-    $scope.filesChanged = function (element){
-        $scope.file = element.files[0];
-        $scope.$apply();
-    };
 
-    $scope.uploadPhoto = function(){
-        var data = new FormData();
-        data.append('userId', $rootScope.uid);
-        data.append('albumId', $scope.momentSelection);
-        data.append('fileToUpload[]',$scope.file);
 
-        console.log(data);
-
-        // var photo = {
-        //     caption: $scope.photoCaption,
-        //     description: $scope.photoDescription,
-        //     location: $scope.photoLocation,
-        //     fileName: $scope.file.name
-        // };
-        //
-        // var config = {
-        //     transformRequest:angular.identity,
-        //     headers: {
-        //         'Content-Type': undefined
-        //     }
-        // };
-        // $http.post('http://imageshare.io/api/v1/uploadimage.php',data,config).success(function (data) {
-        //     if (data.error) {
-        //         console.log("Photo: " + data.message);
-        //     } else {
-        //         // photo.imageId = getPhotoID($scope.momentSelection,$scope.file.name);
-        //         updatePhotoInformation($scope.momentSelection,photo);
-        //         console.log("Successfully uploaded!");
-        //     }
-        // }).error(function () {
-        //
-        // });
-    };
 
     var updatePhotoInformation = function(albumId,photo){
 
