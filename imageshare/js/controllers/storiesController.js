f52c4f9365733ab85a4db1f811dff897b916bdcc
oceas <scott8513@gmail.com>
2016-08-04 10:51:27 -0400

    Implented deleting of stories.

--- imageshare/js/controllers/storiesController.js
+++ imageshare/js/controllers/storiesController.js
@@ -2,6 +2,50 @@ angular.module('imageshare').controller('storiesController', ['$rootScope','$sco
 
     $scope.stories = {};
 
+    var storiesToDelete = {};
+
+    $scope.setToBeDeleted = function(story){
+        if(storiesToDelete[story]){
+            storiesToDelete[story] = false;
+        }else{
+            storiesToDelete[story] = true;
+        }
+    };
+
+    $scope.isToBeDeleted = function(story){
+        return storiesToDelete[story] == true;
+    };
+
+    $scope.delete = function(){
+        for(var storyId in storiesToDelete){
+            deleteStory(storyId);
+        }
+    };
+
+    var deleteStory = function(storyId){
+        var data = $.param({
+            userId: $rootScope.uid,
+            storyId: storyId
+        });
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+        $http.post('http://imageshare.io/api/v1/deletestory.php', data, config)
+            .success(function (data) {
+                if (data.error) {
+                    alert("Stories: " + data.message);
+                } else {
+
+                }
+            })
+            .error(function () {
+
+            });
+    };
+
     var loadStories = function() {
         var data = $.param({
             userId: $rootScope.uid,
---
+++ imageshare/js/directives/storyDelete.js
@@ -0,0 +1,6 @@
+angular.module('imageshare').directive('storyDelete', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '../../partials/pages/stories/delete.html',
+    };
+});
